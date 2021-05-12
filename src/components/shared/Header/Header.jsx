58e5e7eaef73de25c11a432df05184341931f90d
Pilar <pbarkerit@gmail.com>
2021-05-12 00:47:40 -0400

    did some styling for stress relief

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -6,11 +6,15 @@ const Header = () => {
     return (
         <header className='header'>
             <div className='header-left'>
-                <NavLink className='logo' to="/">Logogoeshere</NavLink>
+                <NavLink className='logo' to="/"><img src="https://res.cloudinary.com/dhj9oydvn/image/upload/v1620791546/movie_logo_fdfhcu.png" alt="Movie logo" id="movieLogo"></img></NavLink>
             </div>
             <div className='header-right'>
                 <NavLink className='link' to="/charts">Charts</NavLink>
             </div>
+            <div className='h-bottom-border'><img src='https://res.cloudinary.com/dhj9oydvn/image/upload/v1620794372/wavy_line_rcs1z4.png' alt='Bottom border'
+            id='border-image' ></img>
+            </div>
+            
         </header>
     );
 };
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -11,40 +11,41 @@ const Home = () => {
     const [fetchMovies, setFetchMovies] = useState(false);
     const [loading, setLoading] = useState(true);
 
-    const apiKey = process.env.REACT_APP_MOVIE_API_KEY
-    const search = searchValue => {
-        setLoading(true);
-    }
+    // const apiKey = process.env.REACT_APP_MOVIE_API_KEY
 
-    useEffect(() => {
-        const getMovies = async () => {
-            const moviesApiURL = `https://www.omdbapi.com/?apikey=${apiKey}&`;
-            const response = await axios.get(moviesApiURL, {
-                headers: {
-                    Authorization: `Bearer ${process.env.REACT_APP_MOVIE_API_KEY}`,
-                },
-            });
-            setMoviesData(response.data.records);
-        };
-        getMovies();
-    }, []);
+    // useEffect(() => {
+    //     const getMovies = async () => {
+    //         const moviesApiURL = `https://www.omdbapi.com/?apikey=${apiKey}&`;
+    //         const response = await axios.get(moviesApiURL, {
+    //             headers: {
+    //                 Authorization: `Bearer ${process.env.REACT_APP_MOVIE_API_KEY}`,
+    //             },
+    //         });
+    //         setMoviesData(response.data.records);
+    //     };
+    //     getMovies();
+    // }, []);
 
-    const getMovie = async () => {
-        const movieApiURL = `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchValue}`;
-        const response = await axios.get(movieApiURL, {
-            headers: {
-                Authorization: `Bearer ${process.env.REACT_APP_MOVIE_API_KEY}`,
-            },
-        });
-        setMoviesData(response.data.records);
-    };
-    getMovie();
+    // const getMovie = async () => {
+    //     const movieApiURL = `https://www.omdbapi.com/?apikey=${apiKey}&t=${searchValue}`;
+    //     const response = await axios.get(movieApiURL, {
+    //         headers: {
+    //             Authorization: `Bearer ${process.env.REACT_APP_MOVIE_API_KEY}`,
+    //         },
+    //     });
+    //     setMoviesData(response.data.records);
+    // };
+    // getMovie();
+
+    const handleSubmit = e => e.preventDefault();
+
 
     return (
         <Layout>
             <div className="home">
                 <h1>This is home</h1>
-                <Search search={search} />
+                <Search onSubmit={handleSubmit}
+                />
             </div>
 
         </Layout>
